144f5ca27d199fcaf8c725f24163cf9f30752ab5
teagrain <pierrick.gorisse@gmail.com>
2018-10-15 21:38:09 +0200

    fin TP4

---
+++ tp-04/todo-app/README.md
@@ -0,0 +1,19 @@
+This is a Symfony flex skeleton for a minimal Symfony ToDo app
+project, used for teaching CSC4101 at Telecom SudParis.
+
+Source code can be found at
+https://github.com/olberger/tspcsc4101-todo-skeleton
+
+Composer packages can be found at
+https://packagist.org/packages/oberger/tspcsc4101-todo-skeleton
+
+To test, use :
+ $ composer create-project oberger/tspcsc4101-todo-skeleton todo-app v4.*
+
+Changelog :
+ - v4.x : Add knp_menu and boostrap
+ - v3.x : Add templating with Twig
+ - v2.x : Add basic Web interface
+ - v1.x : initial version with CLI commands
+ 
+-- Olivier Berger
---
+++ tp-04/todo-app/src/Controller/TodoController.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Gestion de la page d'accueil de l'application
+ *
+ * @copyright  2017 Telecom SudParis
+ * @license    "MIT/X" License - cf. LICENSE file at project root
+ */
+
+namespace App\Controller;
+
+use App\Entity\Todo;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Controleur Todo
+ * @Route("/todo")
+ */
+class TodoController extends Controller
+{    
+    /**
+     * Lists all todo entities.
+     * @Route("/", name = "todo_home", methods="GET")
+     * @Route("/list", name = "todo_list", methods="GET")
+     * @Route("/index", name="todo_index", methods="GET")
+     */
+    public function listAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        
+        $todos = $em->getRepository(Todo::class)->findAll();
+        
+        return $this->render('todo/index.html.twig', array(
+            'todos' => $todos,
+        ));
+    }
+    /**
+     * Lists all active todo entities.
+     *
+     * The todo entities which aren't yet completed
+     *
+     * @Route("/list-active", name = "todo_active_list", methods="GET")
+     */
+    public function activelistAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        
+        $todos = $em->getRepository(Todo::class)->findByCompleted(false);
+        
+        return $this->render('todo/active-index.html.twig', array(
+            'todos' => $todos,
+        ));
+    }
+    /**
+     * Finds and displays a todo entity.
+     *
+     * @Route("/{id}", name="todo_show", requirements={ "id": "\d+"}, methods="GET")
+     */
+    public function showAction(Todo $todo): Response
+    {
+        return $this->render('todo/show.html.twig', array(
+            'todo' => $todo,
+        ));
+    }
+}
